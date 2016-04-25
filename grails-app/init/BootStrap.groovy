067dec3d7017f4cfca2b5a6df2c6e30f292a62f4
112921482 <dy0609@qq.com>
2016-04-25 14:13:37 +0800

    初始化用户及其权限

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,21 @@
+import dingdong.Role
+import dingdong.User
+import dingdong.UserRole
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {
+        def adminRole = new Role('ROLE_ADMIN').save()
+        def userRole = new Role('ROLE_USER').save()
+        def testUser = new User('me', 'password').save()
+        UserRole.create testUser, adminRole
+        UserRole.withSession {
+            it.flush()
+            it.clear()
+        }
+        assert User.count() == 1
+        assert Role.count() == 2
+        assert UserRole.count() == 1
     }
     def destroy = {
     }
