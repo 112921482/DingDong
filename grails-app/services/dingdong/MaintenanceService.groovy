a375a365a9b2cc12eb075b349251a7dce76b251e
112921482 <dy0609@qq.com>
2016-05-06 08:49:26 +0800

    搞定

--- grails-app/services/dingdong/MaintenanceService.groovy
+++ grails-app/services/dingdong/MaintenanceService.groovy
@@ -7,21 +7,41 @@ import grails.transaction.Transactional
 @Transactional
 class MaintenanceService {
 
+    /**
+     * 保存菜单
+     * @param menuName 菜名
+     * @param menuPrice 价格
+     * @param savePathList 图片列表
+     * @return
+     */
     def saveMenu(String menuName, BigDecimal menuPrice, String[] savePathList) {
+        Boolean rs = true
         MealMenu mealMenu = new MealMenu(
                 name: menuName,
                 price: menuPrice
         )
-        mealMenu.save()
-        savePathList.each { savePath ->
-            MealPic mealPic = new MealPic(
-                    picUrl: savePath,
-                    mealMenu: mealMenu
-            )
-            mealPic.save()
+        if (!mealMenu.save()) {
+            rs = false
+        } else {
+            savePathList.each { savePath ->
+                MealPic mealPic = new MealPic(
+                        picUrl: savePath,
+                        mealMenu: mealMenu
+                )
+                mealPic.save()
+                if (!mealPic.save()) {
+                    rs = false
+                    return false
+                }
+            }
         }
     }
 
+    /**
+     * 删除菜单
+     * @param id 菜单ID
+     * @return
+     */
     def deleteMenu(Long id) {
         Boolean rs = true
         MealMenu mealMenu = MealMenu.get(id)
