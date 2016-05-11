eba1034440a71e20f98328b93aa305f2364befd3
112921482 <dy0609@qq.com>
2016-05-11 16:00:25 +0800

    发布菜单详情自动排序

--- grails-app/domain/dingdong/meal/ReleaseMenu.groovy
+++ grails-app/domain/dingdong/meal/ReleaseMenu.groovy
@@ -9,6 +9,7 @@ class ReleaseMenu {
 
     static mapping = {
         type comment: "菜单种类（无限制-0，早餐-1，中餐-2，晚餐-3，宵夜-4）"
+        releaseMenuDetails sort: 'id', order: 'desc'
         releaseDate comment: "发布时间"
     }
 
