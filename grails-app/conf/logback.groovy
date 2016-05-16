7fa9ca2d7b98c4695701356e3643ce32f0ccec15
112921482 <dy0609@qq.com>
2016-05-16 10:02:19 +0800

    日志配置

--- grails-app/conf/logback.groovy
+++ grails-app/conf/logback.groovy
@@ -2,22 +2,28 @@ import grails.util.BuildSettings
 import grails.util.Environment
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
+// 控制台日志级别控制
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%level %logger - %msg%n"
     }
 }
+// 日志级别 TRACE < DEBUG < INFO < WARN < ERROR，默认只打印ERROR
+root(INFO, ['STDOUT'])
 
-root(ERROR, ['STDOUT'])
-
+// 日志输出控制
+// 日志路径
 def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
-    appender("FULL_STACKTRACE", FileAppender) {
-        file = "${targetDir}/stacktrace.log"
-        append = true
-        encoder(PatternLayoutEncoder) {
-            pattern = "%level %logger - %msg%n"
-        }
+// 项目名称
+def projectName = "dingdong"
+// 日志日期
+def byDay = timestamp("yyyyMMdd")
+
+appender("STDOUT", FileAppender) {
+    file = "${targetDir}/${projectName}_${byDay}.log"
+    append = true
+    encoder(PatternLayoutEncoder) {
+        pattern = "%level %logger - %msg%n"
     }
-    logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
 }
+root(INFO, ['STDOUT'])
