ce979e382044a1967522cf0e1f2665a3414767f1
112921482 <dy0609@qq.com>
2016-05-19 14:23:01 +0800

    去掉不必要的警告

--- grails-app/controllers/dingdong/WeChatMarketInterceptor.groovy
+++ grails-app/controllers/dingdong/WeChatMarketInterceptor.groovy
@@ -10,7 +10,7 @@ class WeChatMarketInterceptor {
             //没有code则不跳转，进行微信授权验证
             def requestUrl = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=APPID&redirect_uri=REDIRECT_URI&response_type=code&scope=SCOPE#wechat_redirect"
             def redirectUrl = request.getRequestURL().toString()
-            requestUrl = requestUrl.replace("APPID", grailsApplication.config.weixin.appId)
+            requestUrl = requestUrl.replace("APPID", grailsApplication.config.weixin.appId.toString())
             requestUrl = requestUrl.replace("REDIRECT_URI", redirectUrl)
             requestUrl = requestUrl.replace("SCOPE", "snsapi_userinfo")
             redirect(url: requestUrl)
